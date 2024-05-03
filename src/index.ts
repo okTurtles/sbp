85c830369d0d8f27e025db06969972d5975cdfaa
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2024-05-03 16:08:46 +0000

    Implement star selector

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,18 @@ const DOMAIN_REGEX = /^[^/]+/
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function sbp (selector: string, ...data: unknown[]): any {
   const domain = domainFromSelector(selector)
-  if (!selectors[selector]) {
-    throw new Error(`SBP: selector not registered: ${selector}`)
+  const starSelector = `${domain}/*`
+  const selExists = !!selectors[selector]
+  // Copy of the original selector
+  let sel = selector
+  if (!selExists) {
+    // If the selector doesn't exist and the start selector is defined,
+    // use the star selector
+    if (selectors[starSelector]) {
+      sel = starSelector
+    } else {
+      throw new Error(`SBP: selector not registered: ${selector}`)
+    }
   }
   // Filters can perform additional functions, and by returning `false` they
   // can prevent the execution of a selector. Check the most specific filters first.
@@ -29,7 +39,12 @@ function sbp (selector: string, ...data: unknown[]): any {
       }
     }
   }
-  return selectors[selector].call(domains[domain].state, ...data)
+  if (!selExists) {
+    // When using the star selector, the first argument is the original selector
+    // used when calling `sbp`.
+    data.unshift(selector)
+  }
+  return selectors[sel].apply(domains[domain].state, data)
 }
 
 export function domainFromSelector (selector: string): string {
